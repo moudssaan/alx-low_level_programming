8e4d8b87789ff3e75099443cb6c7cabdcd7d7a93
moudssaan <moudssaan@gmail.com>
2023-03-22 16:56:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  * Return: lastDigit
  */
 
-int print_last_digit(int n)
+int 'print_last_digit'(int n)
 {
 	int lastDigit;
 
 	if (n < 0)
-		lastDigit = -1 * (n % 10)
+		lastDigit = -1 * (n % 10);
 	else
 		lastDigit = n % 10;
 	_putchar(lastDigit + '0');
